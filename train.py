b203209c9450f78f26d79203f45b0f09f8dba34c
Collin Brohm <152938275+collinbrohm@users.noreply.github.com>
2025-04-06 07:23:50 -0400

    started training

---
+++ train.py
@@ -0,0 +1,71 @@
+"""Module to train our data."""
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.compose import ColumnTransformer
+
+df = pd.read_csv('filtered_data/combined_cleaned_data.csv')
+
+# NOTE: We can change the numbers as needed, meant to normalize the data. 
+df['demand_score'] = (
+    df['Quantity'] + 
+    df['Sell Price'] / df['Sell Price'].mean() * 1.5 + 
+    df['Rating'] * 2
+)
+
+
+
+df['demand_level'] = pd.qcut(
+    df['demand_score'], q=[0, .34, .67, 1], labels=['low', 'medium', 'high']
+)
+
+df['Date'] = pd.to_datetime(df['Date'])
+df['Month'] = df['Date'].dt.month
+df['Year'] = df['Date'].dt.year
+df['DayOfWeek'] = df['Date'].dt.dayofweek
+
+cat_cols = ['Category', 'Season', 'Location', 'Month', 'DayOfWeek', 'Year']
+num_cols = ['Quantity', 'Sell Price', 'Discount Price', 'Discount %', 'Rating']
+
+# NOTE: Columns we dont need to train on 
+dropped_cols = ['Purchase ID', 'Item Title', 'Description', 'Date', 'Review', 'demand_score', 'demand_level', 'Dataset']
+
+# NOTE: Define logistical regression steps
+
+X = df.drop(dropped_cols, axis=1)
+y = df['demand_level']
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), num_cols),
+        ('cat', OneHotEncoder(handle_unknown='ignore'), cat_cols)
+    ])
+
+
+reg_model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('classifier', LogisticRegression(max_iter=1000, multi_class='multinomial'))
+])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the model
+reg_model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = reg_model.predict(X_test)
+
+# Evaluate model
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+
+
